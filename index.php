339c6a9c2530b581072dacc0d0b2a1042741a1f4
Bekono4526 <bekonorosy0@gmail.com>
2024-05-28 09:41:18 +0200

    log

--- index.php
+++ index.php
@@ -2,5 +2,10 @@
 require_once 'vendor/autoload.php';
 require_once 'config/database.php';
 
+// Inclure le header en haut du fichier pour qu'il soit affiché en premier
+include('header.php');
+
 $router = new \App\Router();
 $router->handleRequest($_SERVER['REQUEST_URI']);
+?>
+
---
+++ src/views/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mon Projet PHP</title>
+    <link rel="stylesheet" href="style.css"> <!-- Lien vers votre fichier CSS -->
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="register.php">Register</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
