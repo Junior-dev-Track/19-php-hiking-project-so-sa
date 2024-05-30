0bf7799143d1af3db83f9ec955951197a772b683
Bekono4526 <bekonorosy0@gmail.com>
2024-05-30 17:02:59 +0200

    mail

--- config/Mail.php
+++ config/Mail.php
@@ -2,13 +2,9 @@
 
 // src/config/mail.php
 return [
-    'host' => 'smtp.gmail.com',          // Replace with your SMTP server host
-    'smtp_auth' => true,                   // Enable SMTP authentication
-    'username' => 'bekonorosy0@gmail.com', // Replace with your SMTP username
-    'password' => '',         // Replace with your SMTP password
-    'smtp_secure' => 'tls',                // Set the security type (tls or ssl)
-    'port' => 587,                         // Set the SMTP server port (587 for tls, 465 for ssl)
-    'from_email' => 'bekonorosy0@gmail.com', // Replace with the sender's email address
+    'domain' => 'YOUR_MAILGUN_DOMAIN',  // Replace with your Mailgun domain
+    'api_key' => 'YOUR_MAILGUN_API_KEY', // Replace with your Mailgun API key
+    'from_email' => 'YOUR_EMAIL@example.com', // Replace with the sender's email address
     'from_name' => 'Hiking-project', // Replace with the sender's name or project name
 ];
 
